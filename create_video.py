a55bd8c1c289300adf98af4c1efba60f34ca7879
Zeph Y <zafriryerushalmi@merlinlabs.com>
2021-10-24 14:11:55 -0700

    Fixed fps calculation

--- create_video.py
+++ create_video.py
@@ -14,8 +14,10 @@ dataset_dir_path = '../DataSets/Video/AU-AIR-2019/'
 annotations_path = dataset_dir_path + 'auair2019annotations/'
 image_path = dataset_dir_path + 'auair2019data/images/'
 annotations_file = annotations_path + 'annotations.json'
-image_files = image_path + '*.jpg'
+
 video_ext = '.avi'
+image_ext = '.jpg'
+image_files = image_path + '*' + image_ext
 
 
 def CollectingImages(count, total_cnt):
@@ -84,22 +86,23 @@ def CreateVideo(images, fps, video_name, video_ext, video_frame_limit=3000):
 #     df_tmp = pd.DataFrame(s.tolist(), columns=['head', 'time', 'x', 'id.jpg'])
 #     s = pd.Series(df_tmp['id.jpg'].str.split('.'))
 #     df_tmp2 = pd.DataFrame(s.tolist(), columns=['id', 'jpg'])
-#     df['time'] = df_tmp['time']
-#     df['id'] = df_tmp2['id']
-#     df.set_index('image_name')
+#     df_full['time'] = df_tmp['time']
+#     df_full['id'] = df_tmp2['id']
+#     df_full.set_index('image_name')
 
 
 # using annotations:
 print("Loading annotations...")
 data = json.load((open(annotations_file)))
-df_ann = pd.json_normalize(data, 'annotations').sort_values(by=['image_name'])
-# df_ann = pd.json_normalize(
-#     data, ['annotations', 'bbox']).sort_values(by=['image_name'])
-print(df_ann)
+df_json = pd.json_normalize(data, 'annotations').sort_values(by=['image_name'])
+df_bbox = pd.json_normalize(
+    data, ['annotations', 'bbox'])
+print(len(df_json), len(df_bbox))
+print(df_bbox)
 print(
-    f'Number for annotation duplicates is {df_ann.image_name.duplicated().sum()}')
+    f'Number for annotation duplicates is {df_json.image_name.duplicated().sum()}')
 
-# df_ann_date = df_ann[df_ann['image_name'].str.contains(search_date)]
+# df_json_date = df_json[df_json['image_name'].str.contains(search_date)]
 
 # using images:
 print("Finding image names...")
@@ -119,51 +122,56 @@ print(
 
 # df_imgs = df_imgs.sort_values(by=['time', 'id']).drop_duplicates(
 #     subset=['time', 'id'])
-# year = df_ann['time.year']
-# month = df_ann['time.month']
-# day = df_ann['time.day']
-# min = df_ann['time.min']
-# sec = df_ann['time.sec']
-# msec = df_ann['time.ms']
+# year = df_json['time.year']
+# month = df_json['time.month']
+# day = df_json['time.day']
+# min = df_json['time.min']
+# sec = df_json['time.sec']
+# msec = df_json['time.ms']
 # print(year.values)
-# df_ann['datetime'] = pd.Timestamp(
+# df_json['datetime'] = pd.Timestamp(
 #     str(year.values) + '-' + str(month.values) + '-' + str(day.values))
 # # 'time.year', 'time.month', 'time.day', 'time.hour', 'time.min', 'time.sec')
 
-# print(df_ann['datetime'])
+# print(df_json['datetime'])
 
 
-df_merge = pd.merge(df_ann, df_imgs, how='inner', on='image_name')
-df_grp = df_merge.groupby(
+df_full = pd.merge(df_json, df_imgs, how='inner', on='image_name')
+df_grp = df_full.groupby(
     by=['time.year', 'time.month', 'time.day', 'time.hour', 'time.min', 'time.sec'])
 print("Date grouping: /n")
 print(df_grp.size())
 
-unique_dates = unique(df_merge['time'])
+unique_dates = unique(df_full['time'])
 video_counts = len(unique_dates)
 print(f"There are {video_counts} video recordings:")
 print(unique_dates)
 
 
 # do for loop here on date
-for i in range(0, 1):
+# for i in range(0, video_counts):
+for i in range(0, video_counts):
     search_date = unique_dates[i]
     print('####### START #########')
     print(f'Video # {i + 1} / {video_counts}')
     print(f'Selecting date: {search_date}')
-    df_merge_date = df_merge[df_merge['image_name'].str.contains(search_date)]
-    images = pd.Series(df_merge_date['full_path'].values,
-                       index=df_merge_date['full_path'])
+    df_date = df_full[df_full['image_name'].str.contains(search_date)]
+    images = pd.Series(df_date['full_path'].values,
+                       index=df_date['full_path'])
 
-    MSEC_2_SEC = 0.001
-    # ((df_merge_date['time.ms'].max() - df_merge_date['time.ms'].min())/len(df_merge_date)) * MSEC_2_SEC
-    fps = 15
+# Calculate Frames Per Second
+    timespan_ms = df_date['time.ms'].iloc[-1] - df_date['time.ms'].iloc[0]
+    timespan_sec = timespan_ms/1000
+    print(
+        f'Recording duration is {timespan_sec} sec')
+    fps = len(df_date)/timespan_sec
     print(f'fps: {fps}')
+
     if(make_video):
         video_name = 'video_' + search_date
         if os.path.exists(video_name + video_ext):
             os.remove(video_name + video_ext)
 
-        CreateVideo(images, fps, video_name, video_ext, 3000)
+        CreateVideo(images, fps, video_name, video_ext, 2000)
 
     print('######## END ##########')
