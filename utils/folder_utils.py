ec29374abd9b9113ce05eb0f53880117c400894f
zyerusha <zafrir.y@gmail.com>
2021-12-28 23:36:19 -0800

    added CopyFolders(src, dest)

--- utils/folder_utils.py
+++ utils/folder_utils.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import shutil
 
 class FolderUtils:
     '''Utilities supporting files and folders'''
@@ -15,3 +16,10 @@ class FolderUtils:
             path_to_file = os.path.join(dir, file)
             os.remove(path_to_file)
 
+    [staticmethod]
+    def CopyFolders(src, dest):
+        ''' This is a helper function to copy entire folders'''
+        if os.path.exists(dest): 
+            shutil.rmtree(dest) 
+        shutil.copytree(src,dest)
+        print(f'Copied folder {src} --> {dest}')
